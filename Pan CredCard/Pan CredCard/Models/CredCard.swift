10a91b8b7ef5e15e27f42fec3a48fdbf649200c9
Michael Bressiani <>
2024-01-20 15:11:12 -0300

    Incluindo o modelo de cartão

---
+++ Pan CredCard/Pan CredCard/Models/CredCard.swift
@@ -0,0 +1,20 @@
+//
+//  CredCard.swift
+//  Pan CredCard
+//
+//  Created by Michael Bressiani on 20/01/24.
+//
+
+import Foundation
+
+struct Cards: Decodable {
+    let cardsList: [[String: Card]]
+}
+
+// MARK: - CardsList
+struct Card: Decodable {
+    let alias: String?
+    let credit: Bool
+    let debit: Bool?
+    let number, codSec, image: String
+}
