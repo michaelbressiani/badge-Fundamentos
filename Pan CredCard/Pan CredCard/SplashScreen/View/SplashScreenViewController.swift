f959455368292fef6a7f4f8c62c51d2031d7713e
Michael Bressiani <>
2024-01-30 19:36:19 -0300

    Ajustando posicionamento da navegação para a camada de View

--- Pan CredCard/Pan CredCard/SplashScreen/View/SplashScreenViewController.swift
+++ Pan CredCard/Pan CredCard/SplashScreen/View/SplashScreenViewController.swift
@@ -3,7 +3,6 @@
 //  Pan CredCard
 //
 //  Created by Michael Bressiani on 20/01/24.
-//
 
 import UIKit
 
@@ -11,20 +10,26 @@ class SplashScreenViewController: UIViewController {
     
     @IBOutlet weak var titleAppLabel: UILabel!
     
-    private var viewModel: SplashScreenViewModel?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.viewModel = SplashScreenViewModel(viewController: self)
         elementsConfig()
-        viewModel?.navigationListCredCards()
+        navigationListCredCards()
     }
     
-    func elementsConfig() {
+    public func elementsConfig() {
         titleAppLabel.text = "Pan CredCard"
         titleAppLabel.textColor = .white
         titleAppLabel.font = UIFont.systemFont(ofSize: 40, weight: .bold)
         view.backgroundColor = UIColor(red: 0/255.0, green: 176/255.0, blue: 240/255.0, alpha: 1.0)
+    }
+    
+    public func navigationListCredCards() {
+        let vcString = String(describing: ListCredCardsViewController.self)
+        let vc = UIStoryboard(name: vcString, bundle: nil).instantiateViewController(withIdentifier: vcString) as? ListCredCardsViewController
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+            self.navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
+        })
     }
 }
 
